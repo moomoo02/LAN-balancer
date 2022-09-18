1129311448e726ef4bf8cd96e5c03180327bfa2a
vince nguyen <vincehnguyen@ucsb.edu>
2022-09-18 13:57:56 -0700

    Added func to convert sheets to csv

--- SheetsScraper.py
+++ SheetsScraper.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
+import gspread
+from bs4 import BeautifulSoup
+import csv
+import requests
+import sys
+
+gc = gspread.service_account(filename="creds.json")
+
+sh = gc.open('LAN-balancer-data').sheet1
+
+
+sh.append_row(['first','second'])
 
 class SheetsScraper:
     def __init__(self, sheetUrl):
-        self.sheetURL = sheetUrl
+        self.convertSheetToCsv(sheetUrl)
+
+    #Get the names of everyone on the sheets and returns a list of names
+    def convertSheetToCsv(self,sheetURL):
+        html = requests.get(sheetURL).text
+        soup = BeautifulSoup(html, "lxml")
+        table = soup.find_all("table")[0]
+        index = 0
+
+        
+        with open(str(index) + ".csv", "w") as f:
+            wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+            wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
+            index = index + 1
+
     
 
 
 
-SS = SheetsScraper("Test")
+SS = SheetsScraper("https://docs.google.com/spreadsheets/d/1h3ax4sytEZDduxuduNwzYCiZr4PQu3z332Yp70wWPJU/edit?usp=sharing")
