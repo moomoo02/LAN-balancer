e6a39f575db1ed2a019e6ca2b739a9f1d29304d9
vince nguyen <vincehnguyen@ucsb.edu>
2022-09-18 14:27:01 -0700

    added getNames func

--- SheetsScraper.py
+++ SheetsScraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import gspread
 from bs4 import BeautifulSoup
+import pandas as pd
 import csv
 import requests
 import sys
@@ -15,22 +16,38 @@ sh.append_row(['first','second'])
 class SheetsScraper:
     def __init__(self, sheetUrl):
         self.convertSheetToCsv(sheetUrl)
+        self.data = "data.csv"
 
     #Get the names of everyone on the sheets and returns a list of names
     def convertSheetToCsv(self,sheetURL):
         html = requests.get(sheetURL).text
         soup = BeautifulSoup(html, "lxml")
         table = soup.find_all("table")[0]
-        index = 0
-
         
-        with open(str(index) + ".csv", "w") as f:
+        with open("data.csv", "w") as f:
             wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
             wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
-            index = index + 1
+
+    #Returns a list of names
+    def getNames(self):
+        df = pd.read_csv(self.data, encoding='cp1252')
+        names = df['Name'] #you can also use df['column_name']
+
+        #Filter out nan
+        res = []
+        for name in names:
+            if not pd.isnull(name):
+                res.append(name)
+        return res
+
+
 
     
 
 
 
 SS = SheetsScraper("https://docs.google.com/spreadsheets/d/1h3ax4sytEZDduxuduNwzYCiZr4PQu3z332Yp70wWPJU/edit?usp=sharing")
+names = SS.getNames()
+
+for name in names:
+    print(name)
