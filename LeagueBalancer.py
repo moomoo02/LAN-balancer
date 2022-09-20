7d80980110387fa810c9737a59f030256869edd9
vince nguyen <vincehnguyen@ucsb.edu>
2022-09-20 16:36:07 -0700

    Added getAverageScore

--- LeagueBalancer.py
+++ LeagueBalancer.py
@@ -2,9 +2,19 @@ class LeagueBalancer:
 
     def __init__(self, players={}):
         self.players = players
-
+        self.playersListSize = len(players)
+        self.numberOfTeams = len(players) / 5    
 
     #Returns the list of players and their rank score
     def getPlayers(self):
         return self.players
+
+    #Returns the ideal score of a team
+    def getTeamAverageScore(self):
+        #Compute total score sum
+        scoreSum = 0
+        for ign,score in self.players.items():
+            scoreSum += int(score)
+        
+        return scoreSum / self.numberOfTeams
 
